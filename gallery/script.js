a96e2672e121a3e317b4ba00e8e8fc8378f272e7
amitverma147 <vamit20000@gmail.com>
2024-05-05 01:03:01 +0530

    updated

--- gallery/script.js
+++ gallery/script.js
@@ -1,1 +1,74 @@
-var btn = document.querySelectorAll("button");
+var array = [
+  {
+    name: "roses",
+    image:
+      "https://images.unsplash.com/photo-1550593854-87edb352e3be?q=80&w=1844&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    name: "cats",
+    image:
+      "https://images.unsplash.com/photo-1529778873920-4da4926a72c2?q=80&w=1936&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    name: "dogs",
+    image:
+      "https://images.unsplash.com/photo-1583337130417-3346a1be7dee?q=80&w=1964&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    name: "models",
+    image:
+      "https://images.unsplash.com/photo-1515886657613-9f3515b0c78f?q=80&w=1920&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    name: "models",
+    image:
+      "https://images.unsplash.com/photo-1524504388940-b1c1722653e1?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    name: "web design",
+    image:
+      "https://images.unsplash.com/photo-1547658719-da2b51169166?q=80&w=1964&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    name: "coders",
+    image:
+      "https://plus.unsplash.com/premium_photo-1690303193709-dedfee29c452?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+];
+var overlay = document.querySelector(".overlay");
+var inp = document.querySelector("#searchinput");
+function showCards() {
+  var cultter = "";
+  array.forEach(function (obj) {
+    cultter += `<div class="box">
+        <img class="cursor-pointer" src="${obj.image}" alt="image">
+        <div class="caption">Lorem ipsum </div>
+    </div>`;
+  });
+  document.querySelector(".container").innerHTML = cultter;
+}
+
+function search() {
+  inp.addEventListener("focus", function () {
+    overlay.style.display = "block";
+  });
+  inp.addEventListener("blur", function () {
+    overlay.style.display = "none";
+  });
+  inp.addEventListener("input", function () {
+    const filteredArray = array.filter((obj) =>
+      obj.name.toLowerCase().startsWith(inp.value)
+    );
+    var clutter = "";
+    filteredArray.forEach(function (obj) {
+      clutter += `<div class="res flex px-8 py-3">
+            <i class="ri-search-line font-semibold mr-5"></i>
+            <h3 class="font-semibold">${obj.name}</h3>
+        </div>`;
+    });
+    document.querySelector(".searchdata").style.display = "block";
+    document.querySelector(".searchdata").innerHTML = clutter;
+  });
+}
+search();
+showCards();
